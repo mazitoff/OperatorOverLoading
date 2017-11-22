44aea4b083eb3b842a16b8b3566522c0c4c9ec8b
MSR <mazitov@ukr.net>
2017-11-22 19:53:31 +0200

    Add project files.

---
+++ OperatorOverLoading/Point.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverLoading
+{
+    class Point
+    {
+        public bool IsDrawed { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public static Point operator +(Point x1, Point x2)
+        {
+            return new Point
+            {
+                X = x1.X + x2.X,
+                Y = x1.Y + x2.Y
+            };
+        }
+
+        public static Point operator -(Point x1, Point x2)
+        {
+            return new Point
+            {
+                X = x1.X - x2.X,
+                Y = x1.Y - x2.Y
+            };
+        }
+
+        public static Point operator ++(Point x1)
+        {
+            return new Point
+            {
+                X = x1.X++,
+                Y = x1.Y++
+            };
+        }
+        public static Point operator --(Point x1)
+        {
+            return new Point
+            {
+                X = x1.X--,
+                Y = x1.Y--
+            };
+        }
+
+        public static bool operator >(Point x1, Point x2)
+        {
+            return x1.X > x2.X && x1.Y > x2.Y;
+        }
+
+        public static bool operator <(Point x1, Point x2)
+        {
+            return x1.X < x2.X && x1.Y < x2.Y;
+        }
+
+        public static bool operator true(Point x1)
+        {
+            return x1.IsDrawed;
+        }
+        public static bool operator false(Point x1)
+        {
+            return !x1.IsDrawed;
+        }
+
+        public static implicit operator double(Point p)
+        {
+            return Math.Sqrt(Math.Pow(p.X, 2) + Math.Pow(p.Y, 2));
+        }
+        //public static explicit operator double(Point p)
+        //{
+        //    return Math.Sqrt(Math.Pow(p.X, 2) + Math.Pow(p.Y, 2));
+        //}
+
+        public override string ToString()
+        {
+            return $"X: {X}; Y: {Y}";
+        }
+    }
+}
---
+++ OperatorOverLoading/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverLoading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x1 = new Point
+            {
+                X = 1,
+                Y = 1
+            };
+            var x2 = new Point
+            {
+                X = 2,
+                Y = 3
+            };
+            var x3 = x1 + x2;
+            Console.WriteLine(x3);
+            x3 = x1 - x2;
+            Console.WriteLine(x3);
+            x3 = x1++;
+            Console.WriteLine(x3);
+            x3 = x1--;
+            Console.WriteLine(x3);
+            Console.WriteLine(x1 > x2);
+
+            object a = x1;
+            //Point p = a;
+            Point p = (Point)a;
+            //Point p = (double)a;
+            double a1 = x1;
+
+            Console.ReadKey();
+        }
+    }
+}
